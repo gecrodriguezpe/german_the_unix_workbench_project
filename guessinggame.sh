c1bad469890cf61cd0b102e76e79c528b4735c95
gecrodriguezpe <gecrodriguezpe@unal.edu.co>
2020-08-26 17:12:12 -0500

    This commit has a functional version of guessinggame.sh. The previous commit did not have a functional version of guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -8,11 +8,25 @@ function guess_number_files {
 	echo "Type your guess fo the number of files in the current folder, then press Enter"
 	read user_response
 
-	if [[ $user_response -eq $num_files ]]
-	then 
+	if [[ $user_response -ne $num_files ]]
+	then
+		while [[ $user_response -ne $num_files ]]
+		do
+			if [[ $user_response -gt $num_files ]]
+			then
+				echo "Your guess is above the actual number of files in the current file, please try again"
+				read user_response
+			elif [[ $user_response -lt $num_files ]]
+			then
+				echo "Your guess is bellow the actual number of files in the current file, please try again"
+				read user_response
+			fi
+		done
+		echo "Good Job. You guessed the correct number of files in the currente folder"
+
+	else
 		echo "Good Job. You guessed the correct number of files in the currente folder"
 	fi
-	#echo $num_files
 }
 
 guess_number_files
